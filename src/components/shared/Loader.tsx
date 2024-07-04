ee9b3a08d499ccfaed336aadbb62a25f94ae7919
Umer-Siddiqui <umermun12@gmail.com>
2024-07-05 02:43:32 +0500

    loader

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -2,13 +2,7 @@
 const Loader = () => {
   return (
     <div className="flex-center w-full">
-<<<<<<< HEAD
       <img src="/public/assets/icons/loader.gif" alt="loader" width={50} height={50}/>
-||||||| 7480fa9
-      <img src="../../../public/assets/icons/loader.svg" alt="loader" width={24} height={24}/>
-=======
-      <img src="https://loading.io/assets/mod/spinner/flipball/index.svg" alt="loader" width={24} height={24}/>
->>>>>>> ebd6f2409ec692137b79a50fe28670042a2174dd
 
     </div>
   )
