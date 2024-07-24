2de4bb46bf491b9420cf53dbb0e8cdf722ddc4af
Umer-Siddiqui <umermun12@gmail.com>
2024-07-25 04:42:22 +0500

    hoaj bhai

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -3,7 +3,6 @@ const Loader = () => {
   return (
     <div className="flex-center w-full">
       <img src="/public/assets/icons/loader.svg" alt="loader" width={50} height={50}/>
-
     </div>
   )
 }
